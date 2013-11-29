47b8c31e64acd63256e5c77095f13e9ec299b7a5
Tom Leese <tom@tomleese.me.uk>
2013-11-29 21:45:17 +0000

    Add polyfill for requestAnimationFrame, close #1

--- js/base.js
+++ js/base.js
@@ -1,9 +1,26 @@
 (function() {
+  "use strict";
+
+  // polyfill based on https://gist.github.com/paulirish/1579671
+  var requestAnimationFramePolyfill = function() {
+    var lastTime = 0;
+
+    return function(callback) {
+      var now = Date.now();
+      var nextTime = Math.max(lastTime + 16, now);
+
+      return window.setTimeout(function() {
+        lastTime = nextTime;
+        callback(lastTime);
+      }, nextTime - now);
+    };
+  };
 
   window.requestAnimationFrame = window.requestAnimationFrame ||
                                  window.mozRequestAnimationFrame ||
                                  window.webkitRequestAnimationFrame ||
                                  window.msRequestAnimationFrame ||
-                                 window.oRequestAnimationFrame;
+                                 window.oRequestAnimationFrame ||
+                                 requestAnimationFramePolyfill();
 
 }());
