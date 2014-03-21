8d2f840029fa967eb6f6a15b710f3e1c5bc050d2
Tom Leese <tom@tomleese.me.uk>
2014-03-21 11:16:19 +0000

    Update code formatting
    Ran through CSSLint and JSHint.

--- scripts/fireworks.js
+++ scripts/fireworks.js
@@ -1,160 +1,161 @@
 (function() {
+    "use strict";
 
-  var createColour = function() {
-    var h = Math.floor(360 * Math.random());
-    return "hsl(" + h + ", 80%, 60%)";
-  };
-
-  var playSound = function(name) {
-    var audio = new Audio("sounds/fireworks/" + name + ".wav");
-    audio.play();
-  };
-
-  var playExplosion = function() {
-    playSound("explosion" + Math.floor(2 * Math.random()));
-  };
-
-  var playKick = function() {
-    playSound("kick");
-  };
-
-  var Particle = function(pos, vel, colour, life) {
-    this.pos = pos;
-    this.vel = vel;
-    this.colour = colour || "rgb(255, 255, 255)";
-    this.life = life || 0.8 + 0.6 * Math.random();
-  };
-
-  Particle.prototype.update = function(dt) {
-    this.pos.x += this.vel.x * dt;
-    this.pos.y += this.vel.y * dt;
-    this.vel.y += 10 * dt; // gravity
-    this.life -= dt;
-  };
-
-  Particle.prototype.draw = function(ctx) {
-    ctx.fillStyle = this.colour;
-    ctx.fillRect(this.pos.x - 1, this.pos.y - 1, 2, 2);
-  };
-
-  Particle.prototype.tick = function(ctx, dt) {
-    this.update(dt);
-    this.draw(ctx);
-  };
-
-  var Firework = function(pos, vel) {
-    Particle.call(this, pos, vel);
-    this.particles = [];
-    this.exploded = false;
-  };
-
-  Firework.prototype = Object.create(Particle.prototype);
-
-  Firework.prototype.explode = function() {
-    var colour = createColour();
-
-    for (var angle = 0; angle <= Math.PI * 4; angle += Math.random() * 0.1) {
-      var pos = { x: this.pos.x, y: this.pos.y };
-      var r = Math.random() * 200;
-      var vel = { x: Math.sin(angle) * r, y: Math.cos(angle) * r };
-      vel.x += this.vel.x * 0.25;
-      vel.y += this.vel.y * 0.25;
-
-      this.particles.push(new Particle(pos, vel, colour));
-    }
-
-    playExplosion();
-    this.exploded = true;
-  };
-
-  Firework.prototype.tick = function(ctx, dt) {
-    if (this.life < 0) {
-      if (this.exploded && this.particles.length <= 0) {
-        return false; // delete me!
-      } else if (!this.exploded) {
-        this.explode();
-      }
-    } else {
-      Particle.prototype.tick.call(this, ctx, dt);
-    }
-
-    for (var i = this.particles.length - 1; i >= 0; i--) {
-      this.particles[i].tick(ctx, dt);
-      if (this.particles[i].life < 0) {
-        this.particles.splice(i, 1);
-      }
-    }
-
-    return true;
-  };
-
-  var FireworkLauncher = function(pos) {
-    this.pos = pos;
-    this.fireworks = [];
-  };
-
-  FireworkLauncher.prototype.launch = function(vel) {
-    playKick();
-    var pos = { x: this.pos.x, y: this.pos.y };
-    this.fireworks.push(new Firework(pos, vel));
-  };
-
-  FireworkLauncher.prototype.tick = function(ctx, dt) {
-    ctx.fillStyle = "rgb(255, 255, 255)";
-    for (var i = this.fireworks.length - 1; i >= 0; i--) {
-      if (!this.fireworks[i].tick(ctx, dt)) {
-        this.fireworks.splice(i, 1);
-      }
-    }
-  };
-
-  var canvas = document.getElementById("fireworks-canvas");
-  var ctx = canvas.getContext("2d");
-
-  var launcher = new FireworkLauncher({
-    x: window.innerWidth / 2,
-    y: window.innerHeight
-  });
-
-  var resetCanvas = function() {
-    ctx.fillStyle = "rgb(255, 255, 255)";
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    launcher.pos = { x: window.innerWidth / 2, y: window.innerHeight };
-  };
-
-  var lastTimestep = 0;
-
-  var tick = function(timestep) {
-    var dt = (timestep - lastTimestep) / 1000;
-    lastTimestep = timestep;
-
-    ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-    launcher.tick(ctx, dt);
-    requestAnimationFrame(tick);
-  };
-
-  var addFirework = function(velX, velY) {
-    var vel = {
-      x: (velX - canvas.width / 2),
-      y: -(canvas.height - velY)
+    var createColour = function() {
+        var h = Math.floor(360 * Math.random());
+        return "hsl(" + h + ", 80%, 60%)";
     };
 
-    launcher.launch(vel);
-  };
+    var playSound = function(name) {
+        var audio = new Audio("sounds/fireworks/" + name + ".wav");
+        audio.play();
+    };
 
-  window.addEventListener("resize", resetCanvas);
+    var playExplosion = function() {
+        playSound("explosion" + Math.floor(2 * Math.random()));
+    };
 
-  canvas.addEventListener("mousedown", function(e) {
-    addFirework(e.pageX, e.pageY);
-  });
-  canvas.addEventListener("touchdown", function(e) {
-    addFirework(e.pageX, e.pageY);
-  });
+    var playKick = function() {
+        playSound("kick");
+    };
 
-  resetCanvas();
-  requestAnimationFrame(tick);
+    var Particle = function(pos, vel, colour, life) {
+        this.pos = pos;
+        this.vel = vel;
+        this.colour = colour || "rgb(255, 255, 255)";
+        this.life = life || 0.8 + 0.6 * Math.random();
+    };
+
+    Particle.prototype.update = function(dt) {
+        this.pos.x += this.vel.x * dt;
+        this.pos.y += this.vel.y * dt;
+        this.vel.y += 10 * dt; // gravity
+        this.life -= dt;
+    };
+
+    Particle.prototype.draw = function(ctx) {
+        ctx.fillStyle = this.colour;
+        ctx.fillRect(this.pos.x - 1, this.pos.y - 1, 2, 2);
+    };
+
+    Particle.prototype.tick = function(ctx, dt) {
+        this.update(dt);
+        this.draw(ctx);
+    };
+
+    var Firework = function(pos, vel) {
+        Particle.call(this, pos, vel);
+        this.particles = [];
+        this.exploded = false;
+    };
+
+    Firework.prototype = Object.create(Particle.prototype);
+
+    Firework.prototype.explode = function() {
+        var colour = createColour();
+
+        for (var angle = 0; angle <= Math.PI * 4; angle += Math.random() * 0.1) {
+            var pos = { x: this.pos.x, y: this.pos.y };
+            var r = Math.random() * 200;
+            var vel = { x: Math.sin(angle) * r, y: Math.cos(angle) * r };
+            vel.x += this.vel.x * 0.25;
+            vel.y += this.vel.y * 0.25;
+
+            this.particles.push(new Particle(pos, vel, colour));
+        }
+
+        playExplosion();
+        this.exploded = true;
+    };
+
+    Firework.prototype.tick = function(ctx, dt) {
+        if (this.life < 0) {
+            if (this.exploded && this.particles.length <= 0) {
+                return false; // delete me!
+            } else if (!this.exploded) {
+                this.explode();
+            }
+        } else {
+            Particle.prototype.tick.call(this, ctx, dt);
+        }
+
+        for (var i = this.particles.length - 1; i >= 0; i--) {
+            this.particles[i].tick(ctx, dt);
+            if (this.particles[i].life < 0) {
+                this.particles.splice(i, 1);
+            }
+        }
+
+        return true;
+    };
+
+    var FireworkLauncher = function(pos) {
+        this.pos = pos;
+        this.fireworks = [];
+    };
+
+    FireworkLauncher.prototype.launch = function(vel) {
+        playKick();
+        var pos = {"x": this.pos.x, "y": this.pos.y};
+        this.fireworks.push(new Firework(pos, vel));
+    };
+
+    FireworkLauncher.prototype.tick = function(ctx, dt) {
+        ctx.fillStyle = "rgb(255, 255, 255)";
+        for (var i = this.fireworks.length - 1; i >= 0; i--) {
+            if (!this.fireworks[i].tick(ctx, dt)) {
+                this.fireworks.splice(i, 1);
+            }
+        }
+    };
+
+    var canvas = document.getElementById("fireworks-canvas");
+    var ctx = canvas.getContext("2d");
+
+    var launcher = new FireworkLauncher({
+        x: window.innerWidth / 2,
+        y: window.innerHeight
+    });
+
+    var resetCanvas = function() {
+        ctx.fillStyle = "rgb(255, 255, 255)";
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        launcher.pos = {"x": window.innerWidth / 2, "y": window.innerHeight};
+    };
+
+    var lastTimestep = 0;
+
+    var tick = function(timestep) {
+        var dt = (timestep - lastTimestep) / 1000;
+        lastTimestep = timestep;
+
+        ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        launcher.tick(ctx, dt);
+        window.requestAnimationFrame(tick);
+    };
+
+    var addFirework = function(velX, velY) {
+        var vel = {
+            x: (velX - canvas.width / 2),
+            y: -(canvas.height - velY)
+        };
+
+        launcher.launch(vel);
+    };
+
+    window.addEventListener("resize", resetCanvas);
+
+    canvas.addEventListener("mousedown", function(e) {
+        addFirework(e.pageX, e.pageY);
+    });
+    canvas.addEventListener("touchdown", function(e) {
+        addFirework(e.pageX, e.pageY);
+    });
+
+    resetCanvas();
+    window.requestAnimationFrame(tick);
 
 }());
--- scripts/snow.js
+++ scripts/snow.js
@@ -1,77 +1,78 @@
 (function() {
+    "use strict";
 
-  var Flake = function() {
-    this.radius = Math.random() * 3 + 1;
+    var Flake = function() {
+        this.radius = Math.random() * 3 + 1;
 
-    this.pos = {
-      x: Math.random() * window.innerWidth,
-      y: Math.random() * window.innerHeight,
+        this.pos = {
+            x: Math.random() * window.innerWidth,
+            y: Math.random() * window.innerHeight,
+        };
+
+        this.velocity = { x: 0, y: 0 };
     };
 
-    this.velocity = { x: 0, y: 0 };
-  };
+    Flake.prototype.wrapValues = function() {
+        var v = this.radius * 0.9;
+        this.velocity.y = Math.min(this.velocity.y, v);
+        this.velocity.x = Math.min(Math.max(this.velocity.x, -v / 5), v / 5);
 
-  Flake.prototype.wrapValues = function() {
-    var v = this.radius * 0.9;
-    this.velocity.y = Math.min(this.velocity.y, v);
-    this.velocity.x = Math.min(Math.max(this.velocity.x, -v / 5), v / 5);
+        var height = window.innerHeight + this.radius * 2;
+        if (this.pos.y > window.innerHeight + this.radius) {
+            this.pos.y -= height;
+        }
 
-    var height = window.innerHeight + this.radius * 2;
-    if (this.pos.y > window.innerHeight + this.radius) {
-      this.pos.y -= height;
+        var width = window.innerWidth + this.radius * 2;
+        if (this.pos.x < -this.radius) {
+            this.pos.x += width;
+        }
+
+        if (this.pos.x > window.innerWidth + this.radius) {
+            this.pos.x -= width;
+        }
+    };
+
+    Flake.prototype.tick = function(ctx) {
+        this.velocity.y += 0.4 - 0.5 * Math.random();
+        this.velocity.x += 0.05 - 0.1 * Math.random();
+        this.pos.x += this.velocity.x;
+        this.pos.y += this.velocity.y;
+
+        this.wrapValues();
+
+        ctx.beginPath();
+        ctx.arc(this.pos.x, this.pos.y, this.radius, 0, Math.PI * 2);
+        ctx.fill();
+    };
+
+    var canvas = document.getElementById("snow-canvas");
+    var ctx = canvas.getContext("2d");
+
+    var flakes = [];
+    for (var i = 0; i < 512; i++) {
+        flakes.push(new Flake());
     }
 
-    var width = window.innerWidth + this.radius * 2;
-    if (this.pos.x < -this.radius) {
-      this.pos.x += width;
-    }
+    var resetCanvas = function() {
+        ctx.fillStyle = "rgb(255, 255, 255)";
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+    };
 
-    if (this.pos.x > window.innerWidth + this.radius) {
-      this.pos.x -= width;
-    }
-  };
+    var redraw = function() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  Flake.prototype.tick = function(ctx) {
-    this.velocity.y += 0.4 - 0.5 * Math.random();
-    this.velocity.x += 0.05 - 0.1 * Math.random();
-    this.pos.x += this.velocity.x;
-    this.pos.y += this.velocity.y;
+        ctx.fillStyle = "rgb(255, 255, 255)";
+        for (var i = flakes.length - 1; i >= 0; i--) {
+            flakes[i].tick(ctx);
+        }
 
-    this.wrapValues();
+        window.requestAnimationFrame(redraw);
+    };
 
-    ctx.beginPath();
-    ctx.arc(this.pos.x, this.pos.y, this.radius, 0, Math.PI * 2);
-    ctx.fill();
-  };
+    window.addEventListener("resize", resetCanvas);
 
-  var canvas = document.getElementById("snow-canvas");
-  var ctx = canvas.getContext("2d");
-
-  var flakes = [];
-  for (var i = 0; i < 512; i++) {
-    flakes.push(new Flake());
-  }
-
-  var resetCanvas = function() {
-    ctx.fillStyle = "rgb(255, 255, 255)";
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-  };
-
-  var redraw = function() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    ctx.fillStyle = "rgb(255, 255, 255)";
-    for (var i = flakes.length - 1; i >= 0; i--) {
-      flakes[i].tick(ctx);
-    }
-
-    requestAnimationFrame(redraw);
-  };
-
-  window.addEventListener("resize", resetCanvas);
-
-  resetCanvas();
-  redraw();
+    resetCanvas();
+    redraw();
 
 }());
